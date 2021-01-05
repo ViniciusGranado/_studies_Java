c17200741ac7544e96694165d85df3a81aa2932b
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-01-04 21:38:03 -0300

    Update Exercise23

--- Loiane Groner Java Course/class-17/Exercise23.java
+++ Loiane Groner Java Course/class-17/Exercise23.java
@@ -1,7 +1,7 @@
 public class Exercise23 {
   public static void main (String[] args) {
 
-    for (int i = 0; i < 50; i++) {
+    for (int i = 1; i <= 50; i++) {
       System.out.println((i + 1) + " - R$" + (1.99 * i));
     };
   }
