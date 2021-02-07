b225e1b32ce336d9e6b895acd675b9498d714233
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-07 11:36:01 -0300

    Update Exercise04.java

--- Loiane Groner Java Course/class-24/exercise04/Exercise04.java
+++ Loiane Groner Java Course/class-24/exercise04/Exercise04.java
@@ -1,15 +1,17 @@
 public class Exercise04 {
-  Book book = new Book();
-  book.title = "Fluam, Minhas Lágrimas, Disse o Policial";
-  book.author = "Philip K Dick";
-  book.pages = 256;
-  book.publishYear = 2013;
-  book.publisher = "Aleph";
-  book.language = "Portuguese";
-  book.edition = 1;
-  book.isbn = "8576571307";
-  book.libraryRegistry = "45215478";
-  book.isAvailable = false;
-  book.lastBorrowDate = "05/02/2021";
-  book.lastUserName = "Marcos dos Santos Pereira";
+  public static void main (String[] args) {
+    LivroDeBiblioteca book = new LivroDeBiblioteca();
+    book.title = "Fluam, Minhas Lágrimas, Disse o Policial";
+    book.author = "Philip K Dick";
+    book.pages = 256;
+    book.publishYear = 2013;
+    book.publisher = "Aleph";
+    book.language = "Portuguese";
+    book.edition = 1;
+    book.isbn = "8576571307";
+    book.libraryRegistry = "45215478";
+    book.isAvailable = false;
+    book.lastBorrowDate = "05/02/2021";
+    book.lastUserName = "Marcos dos Santos Pereira"; 
+  }
 }
