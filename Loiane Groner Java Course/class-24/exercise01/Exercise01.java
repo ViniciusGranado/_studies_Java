ab737d13e7f5ebfe3c454e639e04a0c822092102
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-06 23:28:37 -0300

    Added solving to class 24 exercise 01

---
+++ Loiane Groner Java Course/class-24/exercise01/Exercise01.java
@@ -0,0 +1,11 @@
+public class Exercise01 {
+  public static void main (String[] args) {
+    Lampada ourolux = new Lampada();
+    ourolux.brand = "Ourolux";
+    ourolux.model = "Controled";
+    ourolux.type = "Led";
+    ourolux.watts = 9;
+    ourolux.kelvin = 6500;
+    ourolux.price = 25.99;
+  }  
+}
---
+++ Loiane Groner Java Course/class-24/exercise01/Lampada.java
@@ -0,0 +1,8 @@
+public class Lampada {
+  String brand;
+  String model;
+  String type;
+  int watts;
+  int kelvin;
+  double price;
+}
