89160e6f16146c938ec50745bd3a845eb78a83c3
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-07 11:39:22 -0300

    Added solving to class 24 exercise 05

---
+++ Loiane Groner Java Course/class-24/exercise05/BankAccount.java
@@ -0,0 +1,7 @@
+public class BankAccount {
+  String accountNumber;
+  String agencyNumber;
+  double accountBalance;
+  boolean isSpecial;
+  double accountLimit;
+}
---
+++ Loiane Groner Java Course/class-24/exercise05/Exercise05.java
@@ -0,0 +1,10 @@
+public class Exercise05 {
+  public static void main (String[] args) {
+    BankAccount account = new BankAccount();
+    account.accountNumber = "25412";
+    account.agencyNumber = "0236";
+    account.accountBalance = 1520.45;
+    account.isSpecial = false;
+    account.accountLimit = 10000.00;
+  }
+}
