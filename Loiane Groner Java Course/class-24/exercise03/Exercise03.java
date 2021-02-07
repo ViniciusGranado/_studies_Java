306fd43eaa2a7843328cf8937d3a3bfcbf2bbff4
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-07 10:59:33 -0300

    Added solving to class 24 exercise 03

---
+++ Loiane Groner Java Course/class-24/exercise03/Exercise03.java
@@ -0,0 +1,15 @@
+public class Exercise03 {
+  public static void main (String[] args) {
+    Book book = new Book();
+    book.title = "Fluam, Minhas Lágrimas, Disse o Policial";
+    book.author = "Philip K Dick";
+    book.pages = 256;
+    book.publishYear = 2013;
+    book.publisher = "Aleph";
+    book.language = "Portuguese";
+    book.edition = 1;
+    book.isbn = "8576571307";
+    book.price = 150.00;
+    book.hasEbookVersion = true;
+  }
+}
---
+++ Loiane Groner Java Course/class-24/exercise03/LivroLivraria.java
@@ -0,0 +1,12 @@
+public class LivroLivraria {
+  String title;
+  String author;
+  int pages;
+  int publishYear;
+  String publisher;
+  String language;
+  int edition;
+  String isbn;
+  float price;
+  boolean hasEbookVersion;
+}
