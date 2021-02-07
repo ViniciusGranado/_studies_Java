b15a4b3e63dacb2a3708e14000d3fc944be029d1
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-07 11:11:57 -0300

    Added solving to class 24 exercise 04

---
+++ Loiane Groner Java Course/class-24/exercise04/Exercise04.java
@@ -0,0 +1,15 @@
+public class Exercise04 {
+  Book book = new Book();
+  book.title = "Fluam, Minhas Lágrimas, Disse o Policial";
+  book.author = "Philip K Dick";
+  book.pages = 256;
+  book.publishYear = 2013;
+  book.publisher = "Aleph";
+  book.language = "Portuguese";
+  book.edition = 1;
+  book.isbn = "8576571307";
+  book.libraryRegistry = "45215478";
+  book.isAvailable = false;
+  book.lastBorrowDate = "05/02/2021";
+  book.lastUserName = "Marcos dos Santos Pereira";
+}
---
+++ Loiane Groner Java Course/class-24/exercise04/LivroDeBiblioteca.java
@@ -0,0 +1,14 @@
+public class LivroDeBiblioteca {
+  String title;
+  String author;
+  int pages;
+  int publishYear;
+  String publisher;
+  String language;
+  int edition;
+  String isbn;
+  String libraryRegistry;
+  boolean isAvailable;
+  String lastBorrowDate;
+  String lastUserName;
+}
