4efa7486ea156e2e490e573d17cef67569e3d7b0
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-02-28 21:56:20 -0300

    Added solving to class 34 exercise 04

---
+++ Loiane Groner Java Course/class-34/exercise04/ConvertAreaUnits/ConvertAreaUnits.java
@@ -0,0 +1,35 @@
+package exercise04.ConvertAreaUnits;
+
+public class ConvertAreaUnits {
+  public static double meterToFeet (int meters) {
+    return meters * 10.76;
+  }
+
+  public static double meterToFeet (double meters) {
+    return meters * 10.76;
+  }
+
+  public static int feetToCm (int feet) {
+    return feet * 929;
+  }
+
+  public static double feetToCm (double feet) {
+    return feet * 929;
+  }
+
+  public static int mileToAcre (int mile) {
+    return mile * 640;
+  }
+
+  public static double mileToAcre (double mile) {
+    return mile * 640;
+  }
+
+  public static double acreToFeet (int acre) {
+    return acre * 43560;
+  }
+
+  public static double acreToFeet (double acre) {
+    return acre * 43560;
+  }
+}
---
+++ Loiane Groner Java Course/class-34/exercise04/Exercise04.java
@@ -0,0 +1,16 @@
+package exercise04;
+
+import exercise04.ConvertAreaUnits.ConvertAreaUnits;
+
+public class Exercise04 {
+  public static void main (String[] args) {
+    System.out.println(ConvertAreaUnits.meterToFeet(1));
+    System.out.println(ConvertAreaUnits.meterToFeet(2.8));
+    System.out.println(ConvertAreaUnits.feetToCm(1));
+    System.out.println(ConvertAreaUnits.feetToCm(2.8));
+    System.out.println(ConvertAreaUnits.mileToAcre(1));
+    System.out.println(ConvertAreaUnits.mileToAcre(2.8));
+    System.out.println(ConvertAreaUnits.acreToFeet(1));
+    System.out.println(ConvertAreaUnits.acreToFeet(2.8));
+  }
+}
