1cb0c1e35ec2bc5cca267b2fd0c331b094998dc0
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-09-12 18:23:46 -0300

    Added ControladorDeLetrasDigitadas class

--- java_course_residence/jogo-da-forca/ControladorDeLetrasJaDigitadas.java
+++ java_course_residence/jogo-da-forca/ControladorDeLetrasJaDigitadas.java
@@ -5,6 +5,7 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
     public ControladorDeLetrasJaDigitadas ()
     {
         // torna this.letrasJaDigitadas igual ao String vazio
+        this.letrasJaDigitadas = "";
     }
 
     public boolean isJaDigitada (char letra)
@@ -12,41 +13,90 @@ public class ControladorDeLetrasJaDigitadas implements Cloneable
         // percorrer o String this.letrasJaDigitadas e verificar se ele
         // possui a letra fornecida, retornando true em caso afirmativo
         // ou false em caso negativo
+
+        for (int i = 0; i < letrasJaDigitadas.length(); i++) {
+            if (letrasJaDigitadas.charAt(i) == letra) return true;
+        };
+
+        return false;
     }
 
     public void registre (char letra) throws Exception
     {
 		// verifica se a letra fornecida ja foi digitada (pode usar
-		// o método this.isJaDigitada, para isso), lancando uma exceção
+		// o mï¿½todo this.isJaDigitada, para isso), lancando uma exceï¿½ï¿½o
 		// em caso afirmativo.
 		// concatena a letra fornecida a this.letrasJaDigitadas.
+
+        if (this.isJaDigitada(letra)) throw new Exception("Letra ja registrada");
+
+        this.letrasJaDigitadas += letra;
     }
 
     public String toString ()
     {
 		// retorna um String com TODAS as letras presentes em
-		// this.letrasJaDigitadas separadas por vírgula (,).
+		// this.letrasJaDigitadas separadas por vï¿½rgula (,).
+
+        String retorno = "";
+
+        for (int i = 0; i < this.letrasJaDigitadas.length(); i++) {
+            if (i == letrasJaDigitadas.length() - 1) {
+                retorno += this.letrasJaDigitadas.charAt(i);
+                continue;
+            };
+
+            retorno += this.letrasJaDigitadas.charAt(i) + ",";
+        }
+
+        return retorno;
     }
 
     public boolean equals (Object obj)
     {
-        // verificar se this e obj são iguais
+        // verificar se this e obj sï¿½o iguais
+
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (this.getClass() != obj.getClass()) return false;
+
+        ControladorDeLetrasJaDigitadas controlador = (ControladorDeLetrasJaDigitadas) obj;
+
+        return this.letrasJaDigitadas.equals(controlador.letrasJaDigitadas);
     }
 
     public int hashCode ()
     {
         // calcular e retornar o hashcode de this
+
+        int hash = 123;
+
+        hash = 13 * hash + new String(this.letrasJaDigitadas).hashCode();
+
+        return hash;
     }
 
     public ControladorDeLetrasJaDigitadas(
     ControladorDeLetrasJaDigitadas controladorDeLetrasJaDigitadas)
-    throws Exception // construtor de cópia
+    throws Exception // construtor de cï¿½pia
     {
         // copiar c.letrasJaDigitadas em this.letrasJaDigitadas
+
+        if (controladorDeLetrasJaDigitadas == null) throw new Exception("Modelo ausente");
+
+        this.letrasJaDigitadas = controladorDeLetrasJaDigitadas.letrasJaDigitadas;
     }
 
     public Object clone ()
     {
-        // criar uma cópia do this com o construtor de cópia e retornar
+        // criar uma cï¿½pia do this com o construtor de cï¿½pia e retornar
+
+        ControladorDeLetrasJaDigitadas ret = null;
+
+        try {
+            ret = new ControladorDeLetrasJaDigitadas(this);
+        } catch(Exception e) {}
+
+        return ret;
     }
 }
