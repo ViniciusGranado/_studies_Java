b024ecb05f72f10afa3fb05f214160cdd2910a8d
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-02 22:39:44 -0300

    Refactor seller getById instantiate

--- nelio-alves-course/JdbcCourse/src/model/dao/impl/SellerDaoJDBC.java
+++ nelio-alves-course/JdbcCourse/src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+  private Connection conn;
+
+  public SellerDaoJDBC(Connection conn) {
+    this.conn = conn;
+  }
+
   @Override
   public void insert(Seller obj) {
 
@@ -23,7 +36,51 @@ public class SellerDaoJDBC implements SellerDao {
 
   @Override
   public Seller findById(Integer id) {
-    return null;
+    PreparedStatement st = null;
+    ResultSet rs = null;
+
+    try {
+      st = conn.prepareStatement(
+        "SELECT seller.*,department.Name as DepName "
+        +"FROM seller INNER JOIN department "
+        +"ON seller.DepartmentId = department.Id "
+        +"WHERE seller.Id = ?"
+      );
+
+      st.setInt(1, id);
+      rs = st.executeQuery();
+
+      if (rs.next()) {
+        Department dep = instantiateDepartment(rs);
+
+        return instantiateSeller(rs, dep);
+      }
+
+      return null;
+    } catch (SQLException e) {
+      throw new DbException(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+      DB.closeResultSet(rs);
+    }
+  }
+
+  private Department instantiateDepartment(ResultSet rs) throws SQLException {
+    return new Department(
+      rs.getInt("DepartmentId"),
+      rs.getString("DepName")
+    );
+  }
+
+  private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+    return new Seller(
+      rs.getInt("Id"),
+      rs.getString("Name"),
+      rs.getString("Email"),
+      rs.getDate("BirthDate"),
+      rs.getDouble("BaseSalary"),
+      dep
+    );
   }
 
   @Override
