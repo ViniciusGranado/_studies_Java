c67ae79c032c6d585fc209e7972a480f51068200
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-02 23:57:18 -0300

    Added department delete method

--- nelio-alves-course/JdbcCourse/src/application/Program2.java
+++ nelio-alves-course/JdbcCourse/src/application/Program2.java
@@ -3,13 +3,14 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
     System.out.println("=== TEST 1: Department findById ===");
@@ -32,5 +33,11 @@ public class Program2 {
     department.setName("D1");
     departmentDao.update(department);
     System.out.println("Update completed");
+
+    System.out.println("\n=== TEST 5: Department delete ===");
+    System.out.print("Enter id for delete test: ");
+    int id = sc.nextInt();
+    departmentDao.deleteById(id);
+    System.out.println("Delete completed");
   }
 }
