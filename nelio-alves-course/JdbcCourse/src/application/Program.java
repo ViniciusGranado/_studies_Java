de53f91ef141d516aaafe0961b7f774e0d5e858d
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-02 22:02:16 -0300

    Added Department class

--- nelio-alves-course/JdbcCourse/src/application/Program.java
+++ nelio-alves-course/JdbcCourse/src/application/Program.java
@@ -1,12 +1,13 @@
 package application;
 
 import db.DB;
+import model.entities.Department;
 
 import java.sql.Connection;
 
 public class Program {
   public static void main(String[] args) {
-    Connection conn = DB.getConnection();
-    DB.closeConnection();
+    Department obj = new Department(1, "Books");
+    System.out.println(obj);
   }
 }
