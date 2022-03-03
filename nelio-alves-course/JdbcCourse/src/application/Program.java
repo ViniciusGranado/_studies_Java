a53acdec41c512ec9dfc314806d9f652c11f8ae2
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-02 23:15:06 -0300

    Added seller insert method

--- nelio-alves-course/JdbcCourse/src/application/Program.java
+++ nelio-alves-course/JdbcCourse/src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Program {
@@ -22,10 +23,15 @@ public class Program {
       System.out.println(obj);
     }
 
-    System.out.println("\n=== TEST 2: Seller findAll ===");
+    System.out.println("\n=== TEST 3: Seller findAll ===");
     list = sellerDao.findAll();
     for(Seller obj : list) {
       System.out.println(obj);
     }
+
+    System.out.println("\n=== TEST 4: Seller insert ===");
+    Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+    sellerDao.insert(newSeller);
+    System.out.println("Inserted! New id = " + newSeller.getId());
   }
 }
