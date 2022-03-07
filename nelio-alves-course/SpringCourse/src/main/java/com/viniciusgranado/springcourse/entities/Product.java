d4e763e0c5d1916e968eaa854d0c9544c875d1fb
Vinicius Granado <vinicius.r.granado@gmail.com>
2022-03-06 23:20:17 -0300

    Added Category-Product many to many association

--- nelio-alves-course/SpringCourse/src/main/java/com/viniciusgranado/springcourse/entities/Product.java
+++ nelio-alves-course/SpringCourse/src/main/java/com/viniciusgranado/springcourse/entities/Product.java
@@ -19,10 +19,16 @@ public class Product implements Serializable {
   private Double Price;
   private String imgUrl;
 
-  @OneToMany
+  @ManyToMany
+  @JoinTable(
+          name = "tb_product_category",
+          joinColumns = @JoinColumn(name = "product_id"),
+          inverseJoinColumns = @JoinColumn(name = "category_id")
+  )
   private Set<Category> categories = new HashSet<>();
 
-  public Product() {}
+  public Product() {
+  }
 
   public Product(Long id, String name, String description, Double price, String imgUrl) {
     this.id = id;
